0017161066b381d16fe553a0407bcee9cbc58178
Noor E Malaika <142078169+Noor-E-Malaika@users.noreply.github.com>
2023-10-21 13:38:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// Exercise 3.1
+let arr = ["apple", "milk", "bread"];
+console.log (arr);
+arr[2] = "banana";
+console.log(arr[2]);
+
+// Exercise 3.2
+let arr1 = ["apple", "milk", "bread", "guava"];
+console.log (arr1);
+arr1[2] = "banana , egg";
+console.log (arr1 [2])
+arr1.pop ();
+arr1.sort ();
+arr1[0] = "lettuce , carrot";
+console.log (arr1 [0])
